1f476e2bd5c82d63aed0e0e7082e426aede81e93
sanghyun jeon <sh@relaybrand.com>
2023-08-09 13:34:47 +0900

    test : 같은 타입의 bean 여러개 존재 시 조회 에러 test & 같은 타입의 bean 여러개 존재 시 조회 방법 test

--- src/test/java/com/tkdgus/springstudy/beantest/ApplicationContextBasicFindTest.java
+++ src/test/java/com/tkdgus/springstudy/beantest/ApplicationContextBasicFindTest.java
@@ -3,18 +3,25 @@ package com.tkdgus.springstudy.beantest;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.tkdgus.springstudy.AppConfig;
+import com.tkdgus.springstudy.repository.MemerRepository;
+import com.tkdgus.springstudy.repository.MemoryMeberRepository;
 import com.tkdgus.springstudy.service.MemberService;
 import com.tkdgus.springstudy.service.MemberServiceImpl;
+import java.util.Map;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.boot.convert.DurationFormat;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 public class ApplicationContextBasicFindTest {
 
-    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SameConfig.class);
 
 
     @Test
@@ -31,5 +38,36 @@ public class ApplicationContextBasicFindTest {
     void failFindBeanByName() {
         assertThrows(NoSuchBeanDefinitionException.class,
             () -> ac.getBean("xxxx", MemberService.class));
+    }
+
+    @Test
+    @DisplayName("타입으로 조회 시 같은 타입의 Bean이 둘 이상이면 중복 오류가 발생")
+    void findBeanDuplicate(){
+        assertThrows(NoUniqueBeanDefinitionException.class,
+            () -> ac.getBean(MemerRepository.class));
+    }
+
+    @Test
+    @DisplayName("특정 타입의 빈 모두 조회")
+    void findAllBeanByType() {
+        Map<String, MemerRepository> beans = ac.getBeansOfType(MemerRepository.class);
+
+        for (String key : beans.keySet()) {
+            System.out.println("key == " + key + " value == " + beans.get(key));
+        }
+    }
+
+    @Configuration
+    static class SameConfig {
+
+        @Bean
+        public MemerRepository memerRepository1() {
+            return new MemoryMeberRepository();
+        }
+
+        @Bean
+        public MemerRepository memerRepository2() {
+            return new MemoryMeberRepository();
+        }
     }
 }
